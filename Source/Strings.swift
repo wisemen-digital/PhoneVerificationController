a6acf71a0da925c3444e3c8b58a684222740bca4
David Jennes <david.jennes@appwise.be>
2017-11-30 16:21:29 +0100

    fix strings bundle

--- Source/Strings.swift
+++ Source/Strings.swift
@@ -40,9 +40,7 @@ enum L10n {
 
 extension L10n {
   private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    let format = NSLocalizedString(key, tableName: table, bundle: PhoneVerificationController.bundle, comment: "")
     return String(format: format, locale: Locale.current, arguments: args)
   }
 }
-
-private final class BundleToken {}
